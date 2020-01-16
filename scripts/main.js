83569205d1d32bc1341e987866fd33475fc8fc0b
chasar <c.ringstrom@gmail.com>
2020-01-16 22:30:32 +0100

    Lade till placeholder för javascript

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const myHeading = document.querySelector('h1');
+const btn2 = document.querySelector('img');
+const var1 = 3;
+let var2 = 3 === 3;
+if (myHeading !== null) {
+    myHeading.textContent = var2.toString();
+    document.writeln(var2.toString());
+} else {
+    document.writeln('aw det funka ente');
+}
+
+if (btn2){
+    alert('VA VEM HAR SATT EN FUL BILD PÅ MIN HEMSIDA');
+}
+
+
+
