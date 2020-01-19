803f4d5c97548607892782d3fdd8a5f04113a433
chasar <c.ringstrom@gmail.com>
2020-01-19 17:43:25 +0100

    Frågar om användarens namn

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,31 @@
 let myImage = document.querySelector("img")
-let i = 0
+let myHeading = document.querySelector("h1")
+let myButton = document.querySelector("button")
+
+console.log(myHeading)
+if(!localStorage.getItem("name")){
+    setUserName()
+}else{
+    let storedName = localStorage.getItem("name")
+    myHeading.textContent = "Välkommen tillbaks, " + storedName
+}
+
+
+
+
+
+
+
+
+
 let start = new Audio("sounds/start.mp3")
 let end = new Audio("sounds/end.mp3")
 myImage.onmouseenter = function () {
-    console.log("hello: " + i++)
     let mySrc = myImage.getAttribute("src")
     if (mySrc === "images/graphicz.jpg") {
         myImage.setAttribute("src", "images/graphicz-angry.jpg")
     }
 }
-
 myImage.onmouseleave = function () {
     myImage.setAttribute("src", "images/graphicz.jpg")
     end.pause()
@@ -19,3 +35,19 @@ myImage.onclick = function () {
     myImage.setAttribute("src", "images/graphicz-deepfried.jpg")
     end.play()
 }
+console.log(myButton)
+
+
+myButton.onclick = function () {
+    setUserName()
+}
+
+
+function setUserName() {
+    let myName = prompt("Please enter your name.")
+    localStorage.setItem("name", myName)
+    console.log("changing name to " + myName)
+    myHeading.textContent = "Välkommen, " + myName
+}
+
+
