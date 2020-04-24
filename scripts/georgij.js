241dba18065d2ec0a36c7efa5b04307e49707327
Chasarr <c.ringstrom@gmail.com>
2020-04-24 21:00:46 +0200

    modified:   georgij/index.html 	modified:   scripts/georgij.js 	modified:   styles/style.css

--- scripts/georgij.js
+++ scripts/georgij.js
@@ -1,18 +1,34 @@
-let myButton = document.getElementById("button")
+let g_button = document.getElementById("g_button")
+let k_button = document.getElementById("k_button")
 //localStorage.setItem("counter", 0)
 //console.log(localStorage.getItem(counter))
 
-if (!localStorage.getItem("counter")) {
-    localStorage.setItem("counter", 0)
-    myButton.innerHTML = "0"
+if (!localStorage.getItem("georgij_counter")) {
+    localStorage.setItem("georgij_counter", 0)
+    g_button.innerHTML = "0"
 }else{
-    myButton.innerHTML = parseInt(localStorage.getItem("counter"))
+    g_button.innerHTML = parseInt(localStorage.getItem("georgij_counter"))
 }
 
-myButton.onclick = function() {
-    let counter = parseInt(localStorage.getItem("counter")) + 1
+if (!localStorage.getItem("kaiwen_counter")) {
+    localStorage.setItem("kaiwen_counter", 0)
+    k_button.innerHTML = "0"
+}else{
+    k_button.innerHTML = parseInt(localStorage.getItem("kaiwen_counter"))
+}
+
+
+g_button.onclick = function() {
+    let counter = parseInt(localStorage.getItem("georgij_counter")) + 1
     console.log("Number = " + counter)
-    localStorage.setItem("counter", counter)
-    myButton.innerHTML=localStorage.getItem("counter")
+    localStorage.setItem("georgij_counter", counter)
+    g_button.innerHTML = localStorage.getItem("georgij_counter")
+}
+
+k_button.onclick = function() {
+    let counter = parseInt(localStorage.getItem("kaiwen_counter")) + 1
+    console.log("Number = " + counter)
+    localStorage.setItem("kaiwen_counter", counter)
+    k_button.innerHTML = localStorage.getItem("kaiwen_counter")
 
 }
